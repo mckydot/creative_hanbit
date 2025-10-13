69434a54d815da7d76fb600cc431dc94c01cf7dc
mckydot <ckyfirstcom@gmail.com>
2025-10-13 13:59:40 +0900

    회원가입 다음단계 페이지 넘어가는 것까지 구현

--- web/js/register.js
+++ web/js/register.js
@@ -1,18 +1,24 @@
-const email = document.getElementById('email');
-const password = document.getElementById('password');
-const passwordCheck = document.getElementById('passwordCheck');
-const registerBtn = document.getElementById('registerBtn');
-const result = document.getElementById('result');
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const passwordCheck = document.getElementById("passwordCheck");
+const registerBtn = document.getElementById("registerBtn");
+const result = document.getElementById("result");
 
-registerBtn.addEventListener('click', function(e){
-    e.preventDefault();
-    console.log("hello world")
-    const psStr = password.value;
-    const psCheckStr = passwordCheck.value;
+registerBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  console.log("hello world");
+  const psStr = password.value.trim();
+  const psCheckStr = passwordCheck.value.trim();
+  const emailCheck = email.value.trim();
 
-    if(psStr != psCheckStr){
-        result.innerText = "비밀번호가 일치하지 않습니다.";
-    }else{
-        result.innerText = "hello world.";
-    }
-})
+  if (emailCheck == "") {
+    result.innerText = "이메일을 입력해주세요.";
+  } else if (psStr == "") {
+    result.innerText = "비밀번호를 입력해주세요.";
+  } else if (psStr != psCheckStr) {
+    result.innerText = "비밀번호가 일치하지 않습니다.";
+  } else {
+    result.innerText = "hello world.";
+    location.href = "register2.html";
+  }
+});
